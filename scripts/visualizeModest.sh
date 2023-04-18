7f007c731de13ca0911955dc6b42cefd0572faba
Aaron Torgesen <aaron.torgesen@usu.edu>
2023-04-18 00:16:51 -0600

    added toy5, goal for toy5, and 2x2 predicate abstract model

---
+++ scripts/visualizeModest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+path_with_ext=$1
+path_no_ext=${path_with_ext%.modest}
+name_with_ext=${path_with_ext##*/}
+name_no_ext=${path_no_ext##*/}
+
+echo ""
+echo "Clearing old directory $name_no_ext"
+rm -r $name_no_ext
+
+echo ""
+echo "Converting $name_with_ext to .dot $name_no_ext.dot"
+echo "modest mosta $path_with_ext -O $name_no_ext.dot"
+modest mosta $path_with_ext -O $name_no_ext.dot
+echo "Conversion to .dot complete"
+
+echo ""
+echo "Converting $name_no_ext.dot to png $name_no_ext.png"
+echo "dot -Tpng $name_no_ext.dot -o $name_no_ext.png"
+dot -Tpng $name_no_ext.dot -o $name_no_ext.png
+
+echo ""
+echo "Conversion to .png Complete"
+echo "Creating folder $name_no_ext"
+mkdir $name_no_ext
+echo "Moving files to $name_no_ext"
+mv $name_no_ext.dot $name_no_ext
+mv $name_no_ext.png $name_no_ext
+echo "Exiting script"
+echo ""
